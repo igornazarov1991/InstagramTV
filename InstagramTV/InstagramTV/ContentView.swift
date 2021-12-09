e9bbb5d6d91e02d5db922486582160fe62491351
Igor Nazarov <igor.nazarov@greenfieldtech.net>
2021-12-09 21:12:15 +0200

    Add Xcode project.

---
+++ InstagramTV/InstagramTV/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstagramTV
+//
+//  Created by Igor Nazarov on 09.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstagramTV/InstagramTV/InstagramTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramTVApp.swift
+//  InstagramTV
+//
+//  Created by Igor Nazarov on 09.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct InstagramTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
